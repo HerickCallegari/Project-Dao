77acfdd80d7fb51ecd29e67972ff0032e06a9e7c
Herick Campos <herickzeno@gmail.com>
2024-02-02 11:31:40 -0300

    Entidades criadas

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,19 @@
 package application;
 
+import java.util.Date;
+
+import model.entities.Department;
+import model.entities.Seller;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Department dp = new Department(12, "d1");
+		
+		Seller seller = new Seller(1, "herick", "zenoplayer155@gmai.com", new Date(), 3000.0, dp);
+		
+		System.out.println(seller);
+		System.out.println(dp);
 	}
 
 }
