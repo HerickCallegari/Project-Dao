006c2fd6eb427630898485e6d852baf349e1618c
Herick Campos <herickzeno@gmail.com>
2024-02-03 14:26:58 -0300

    Dao's criados

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,19 +1,11 @@
 package application;
 
-import java.util.Date;
-
-import model.entities.Department;
-import model.entities.Seller;
+import model.dao.DaoFactory;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Department dp = new Department(12, "d1");
-		
-		Seller seller = new Seller(1, "herick", "zenoplayer155@gmai.com", new Date(), 3000.0, dp);
-		
-		System.out.println(seller);
-		System.out.println(dp);
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 	}
 
 }
